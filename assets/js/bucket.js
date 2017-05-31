350b860da7e0903bc190407bbcf9721159557008
joshin-run <jahmsc@gmail.com>
2017-05-31 14:37:35 -0600

    Feat (thumbnail) Change hello dev thumbnail link

---
+++ assets/js/bucket.js
@@ -0,0 +1,33 @@
+<div>
+      <div class="row">
+        <div class="col-md-4">.col-md-4</div>
+        <div class="col-md-4">.col-md-4</div>
+        <div class="col-md-4">.col-md-4</div>
+      </div>
+
+        <div className="main">
+          <h1 className="title">Celebrate Life!</h1>
+
+
+            <div className="row">
+              <div className="col-xs-6 col-md-3">
+                <img className="roses" src={roses} alt="Rainbow of Roses" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-6 col-md-3">
+                <img className="runners" src={runners} alt="Runners on Track" />
+              </div>
+            </div>
+            <div class="row">
+              <div class="col-xs-6 col-md-3">
+                <img className="rocks" src={rocks} alt="Pile of rocks" />
+              </div>
+            </div>
+          </div>
+          <div className="chat-window-container">
+          <ChatWindow />
+        </div>
+
+        <Main />
+      </div>
