b4b7d8cdac5e24cea7e012e3cee11595147026df
joshin-run <jahmsc@gmail.com>
2017-08-24 16:02:11 -0600

    Feat (style) attempt to create carousel, half way through.

---
+++ assets/js/banner.js
@@ -0,0 +1,36 @@
+// (function () {
+  var slideIndex = 0;
+  showSlides();
+
+  function showSlides() {
+      var i;
+      var slides = document.getElementsByClassName("mySlides");
+      for (i = 0; i < slides.length; i++) {
+         slides[i].style.display = "none";
+      }
+      slideIndex++;
+      if (slideIndex > slides.length) {slideIndex = 1}
+
+      slides[slideIndex-1].style.display = "block";
+
+      setTimeout(showSlides, 3500); // Change image every 2 seconds
+  }
+// })();
+
+
+
+// // Automatic Slideshow - change image every 3 seconds
+// var myIndex = 0;
+// carousel();
+//
+// function carousel() {
+//     var i;
+//     var x = document.getElementsByClassName("mySlides");
+//     for (i = 0; i < x.length; i++) {
+//        x[i].style.display = "none";
+//     }
+//     myIndex++;
+//     if (myIndex > x.length) {myIndex = 1}
+//     x[myIndex-1].style.display = "block";
+//     setTimeout(carousel, 3000);
+// }
